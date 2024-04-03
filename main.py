6407e33e67563d96df1550a1ab2492d38a6efdc3
Stepkata <katstep@student.agh.edu.pl>
2024-04-03 15:44:59 +0200

    added reinforcement learning classes

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import gymnasium as gym
 from models import ModelType
+from policy_network import Policy_Network
+
+from __future__ import annotations
+
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import torch
+import torch.nn as nn
+from torch.distributions.normal import Normal
+
+plt.rcParams["figure.figsize"] = (10, 5)
+
 
 frozen_lake_env = gym.make(
     ModelType.FROZEN_LAKE,
@@ -7,3 +23,4 @@ frozen_lake_env = gym.make(
     map_name='4x4',
     is_slippery=True
 )
+
