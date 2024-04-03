c251214964f3a494255fc4ce1cf5de0b7c531635
Stepkata <katstep@student.agh.edu.pl>
2024-04-03 16:14:40 +0200

    neural network but it is dumb

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
+
+
 import gymnasium as gym
 from models import ModelType
 from policy_network import Policy_Network
-
-from __future__ import annotations
+from reinforce import REINFORCE
 
 import random
 
@@ -19,8 +21,69 @@ plt.rcParams["figure.figsize"] = (10, 5)
 
 frozen_lake_env = gym.make(
     ModelType.FROZEN_LAKE,
-    desc=None,
+    desc=["SFFF", "FHFH", "FFFH", "HFFG"],
     map_name='4x4',
     is_slippery=True
 )
 
+
+wrapped_env = gym.wrappers.RecordEpisodeStatistics(frozen_lake_env, 50)  # Records episode-reward
+
+total_num_episodes = int(5e3)  # Total number of episodes
+# Observation-space of InvertedPendulum-v4 (4)
+obs_space_dims = 1 #???
+print(obs_space_dims)
+# Action-space of InvertedPendulum-v4 (1)
+action_space_dims = 1 #???
+print(action_space_dims)
+rewards_over_seeds = []
+
+for seed in [1, 2, 3, 5, 8]:  # Fibonacci seeds
+    # set seed
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+    # Reinitialize agent every seed
+    agent = REINFORCE(obs_space_dims, action_space_dims)
+    reward_over_episodes = []
+
+    for episode in range(total_num_episodes):
+        # gymnasium v26 requires users to set seed while resetting the environment
+        obs, info = wrapped_env.reset(seed=seed)
+
+        done = False
+        while not done:
+            action = agent.sample_action(obs)
+            action = int(action*10)%4
+            #print(action)
+
+            # Step return type - `tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]`
+            # These represent the next observation, the reward from the step,
+            # if the episode is terminated, if the episode is truncated and
+            # additional info from the step
+            obs, reward, terminated, truncated, info = wrapped_env.step(action)
+            agent.rewards.append(reward)
+
+            # End the episode when either truncated or terminated is true
+            #  - truncated: The episode duration reaches max number of timesteps
+            #  - terminated: Any of the state space values is no longer finite.
+            done = terminated or truncated
+
+        reward_over_episodes.append(wrapped_env.return_queue[-1])
+        agent.update()
+
+        if episode % 1000 == 0:
+            avg_reward = int(np.mean(wrapped_env.return_queue))
+            print("Episode:", episode, "Average Reward:", avg_reward)
+
+    rewards_over_seeds.append(reward_over_episodes)
+
+rewards_to_plot = [[reward[0] for reward in rewards] for rewards in rewards_over_seeds]
+df1 = pd.DataFrame(rewards_to_plot).melt()
+df1.rename(columns={"variable": "episodes", "value": "reward"}, inplace=True)
+sns.set(style="darkgrid", context="talk", palette="rainbow")
+sns.lineplot(x="episodes", y="reward", data=df1).set(
+    title="REINFORCE for InvertedPendulum-v4"
+)
+plt.show()
